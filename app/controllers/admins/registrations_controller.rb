aa618443527445ecbd7a72fd1388c6ccb19b6807
martinosso <103454347+martinosso@users.noreply.github.com>
2022-04-27 17:56:34 +0200

    Update registrations_controller.rb

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -3,7 +3,7 @@
 module Admins
   class RegistrationsController < Devise::RegistrationsController
     include Accessible
-    skip_before_action :check_user, except: %i[new create]
+    skip_before_action :check_user, except: [:new, :create]
     # before_action :configure_sign_up_params, only: [:create]
     # before_action :configure_account_update_params, only: [:update]
 
