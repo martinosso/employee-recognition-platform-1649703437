0e827f7cab68a847b514180a4481ca9f079b3594
martinosso <103454347+martinosso@users.noreply.github.com>
2022-05-04 21:51:48 +0200

    Create admin_kudos_spec.rb

---
+++ spec/system/admin_kudos_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'the kudo listing and deleting process', type: :system do
+  let(:admin) { create(:admin) }
+  let!(:kudo) { create(:kudo) }
+  
+  before do
+    sign_in admin
+  end
+
+  it 'destroys kudo' do
+    visit admins_kudos_path
+    expect(page).to have_content 'Testtitle'
+    click_link('Destroy')
+    expect(page).to have_content('Kudo was successfully destroyed.')
+  end
+end
