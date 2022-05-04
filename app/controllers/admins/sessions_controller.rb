e9f13f00087209db9beb3fc8564613c3af2b1fc5
martinosso <103454347+martinosso@users.noreply.github.com>
2022-05-04 23:25:10 +0200

    Delete accessivle.rb

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -2,8 +2,6 @@
 
 module Admins
   class SessionsController < Devise::SessionsController
-    include Accessible
-    skip_before_action :check_user, only: :destroy
     # before_action :configure_sign_in_params, only: [:create]
 
     # GET /resource/sign_in
--- app/controllers/employees/registrations_controller.rb
+++ app/controllers/employees/registrations_controller.rb
@@ -2,7 +2,6 @@
 
 module Employees
   class RegistrationsController < Devise::RegistrationsController
-    include Accessible
     # before_action :configure_sign_up_params, only: [:create]
     # before_action :configure_account_update_params, only: [:update]
 
