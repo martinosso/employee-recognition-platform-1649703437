4e2176faf6f6c94946a3a75333ad6ada874c4723
martinosso <103454347+martinosso@users.noreply.github.com>
2022-05-05 17:20:33 +0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  def after_sign_in_path_for(resource)
+    if current_admin
+      admins_root_path
+    end
+  end
   def check_logged_in
     authenticate_employee! if !current_employee && !current_admin
   end
