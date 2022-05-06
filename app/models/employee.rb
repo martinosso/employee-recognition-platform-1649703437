749f3a5790cb27604eba2e8169832c47473e8bbd
martinosso <103454347+martinosso@users.noreply.github.com>
2022-05-06 20:14:29 +0200

    Update employee.rb

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,8 @@
 class Employee < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :number_of_available_kudos, presence: true
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
