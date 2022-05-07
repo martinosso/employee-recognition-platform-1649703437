46e66d49701a8abb4f66aced97ccae364451c8fc
martinosso <103454347+martinosso@users.noreply.github.com>
2022-05-07 20:13:12 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
   resources :kudos
   root to: "kudos#index"
 
-  devise_for :admins, path: 'admins', controllers: { sessions: 'admins/sessions' }
+  devise_for :admins, path: 'admins'
   devise_for :employees, path: 'employees'
 
   namespace :admins do
     resources :kudos, only: %i[index destroy]
     resources :employees, only: %i[index update edit destroy]
-    root 'pages#dashboard'
+    root to: 'pages#dashboard', as: :root
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
