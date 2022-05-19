3214b7bc7a59717d47904eab85c36cffc129fc4e
martinosso <103454347+martinosso@users.noreply.github.com>
2022-05-19 18:55:51 +0200

    Add shoulda-matchers gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -74,3 +74,10 @@ RSpec.configure do |config|
     driven_by(:rack_test)
   end
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
