bac30824e9c32dfaf7c1ced13a0fc272aade5cff
martinosso <103454347+martinosso@users.noreply.github.com>
2022-05-23 22:14:43 +0200

    Associate kudo with a company value

--- app/models/company_value.rb
+++ app/models/company_value.rb
@@ -1,3 +1,4 @@
 class CompanyValue < ApplicationRecord
   validates :title, presence: true, uniqueness: true
+  has_many :kudos, class_name: 'Kudo', foreign_key: 'company_value_id', inverse_of: 'company_value', dependent: :destroy
 end
--- app/models/kudo.rb
+++ app/models/kudo.rb
@@ -1,6 +1,8 @@
 class Kudo < ApplicationRecord
   belongs_to :giver, class_name: 'Employee'
   belongs_to :receiver, class_name: 'Employee'
+  belongs_to :company_value, class_name: 'CompanyValue'
 
   validates :title, :content, presence: true
+  validates :company_value, presence: true
 end
