0b85fe09c029db7b68e19a2d1e7e156751deeb86
martinosso <103454347+martinosso@users.noreply.github.com>
2022-05-23 22:32:24 +0200

    Test fix

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Employee, type: :model do
   it 'returns the email' do
     employee = create(:employee)
 
-    expect(employee.email).to eq 'ftestemail@test.com'
+    expect(employee.email).to eq 'email1@test.com'
   end
 end
