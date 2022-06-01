8da99a2f7ff92a2065891471167d0e39ae991eae
martinosso <103454347+martinosso@users.noreply.github.com>
2022-06-01 23:41:25 +0200

    Create tests for rewards

---
+++ spec/models/reward_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Reward, type: :model do
+  describe 'validations' do
+    subject { build(:reward) }
+
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_uniqueness_of(:title) }
+    it { is_expected.to validate_presence_of(:description) }
+  end
+end
---
+++ spec/system/manage_rewards_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'manage reward process', type: :system do
+  let(:admin) { create(:admin) }
+  
+
+  before do
+    sign_in admin
+    create(:reward)
+    visit admins_rewards_path
+  end
+
+  it 'creates new reward' do
+    click_link('New Reward')
+    fill_in 'Title', with: 'System spec:Test Reward Title'
+    fill_in 'Description', with: 'System spec:Test Description'
+    fill_in 'Price', with: 1.32
+    click_button 'Create Reward'
+    expect(page).to have_content 'System spec:Test Reward Title'
+  end
+
+  it 'edits reward' do
+    click_link('Edit')
+    fill_in 'Title', with: 'Edit:Test Reward Title'
+    click_button 'Update Reward'
+    expect(page).to have_content 'Edit:Test Reward Title'
+  end
+
+  it 'destroys reward' do
+    click_link('Destroy')
+    expect(page).to have_content 'Test Reward Title'
+  end
+end
