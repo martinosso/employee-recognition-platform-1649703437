98a4e318caf2836a80d9fbd3c9d30b379e97c5ee
martinosso <103454347+martinosso@users.noreply.github.com>
2022-06-06 11:23:05 +0200

    Create system spec for rewards

---
+++ spec/system/rewards_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'reward test' do
+  let(:employee) { create(:employee) }
+
+  before do
+    sign_in employee
+    create(:reward)
+  end
+
+  it 'lists rewards' do
+    visit rewards_path
+    expect(page).to have_content(Reward.first.title)
+  end
+
+  it 'shows reward' do
+    visit rewards_path
+    click_link 'Show'
+    expect(page).to have_content(Reward.first.description)
+  end
+end
