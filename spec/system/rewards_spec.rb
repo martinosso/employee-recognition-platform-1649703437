4e49abb5f9c227c9d451195e9a11c41af07e8bc7
martinosso <103454347+martinosso@users.noreply.github.com>
2022-06-09 19:25:19 +0200

    Update rewards_spec.rb

--- spec/system/rewards_spec.rb
+++ spec/system/rewards_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'reward test' do
   let(:employee) { create(:employee) }
-
   before do
     sign_in employee
     create(:reward)
@@ -11,11 +10,13 @@ RSpec.describe 'reward test' do
   it 'lists rewards' do
     visit rewards_path
     expect(page).to have_content(Reward.first.title)
+    expect(page).to have_content(Reward.first.price)
   end
 
   it 'shows reward' do
     visit rewards_path
     click_link 'Show'
     expect(page).to have_content(Reward.first.description)
+    expect(page).to have_content(Reward.first.title)
   end
 end
