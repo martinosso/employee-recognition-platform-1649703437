2afd893e9cd80e7aefda6608d7a4795bb10eb547
martinosso <103454347+martinosso@users.noreply.github.com>
2022-06-14 11:50:43 +0200

    Add callbacks after create and destroy kudo

--- app/models/kudo.rb
+++ app/models/kudo.rb
@@ -4,4 +4,12 @@ class Kudo < ApplicationRecord
   belongs_to :company_value
 
   validates :title, :content, presence: true
+
+  after_create do
+    self.receiver.increment(:earned_points, 100).save
+  end
+
+  after_destroy do
+    self.receiver.decrement(:earned_points, 100).save
+  end
 end
