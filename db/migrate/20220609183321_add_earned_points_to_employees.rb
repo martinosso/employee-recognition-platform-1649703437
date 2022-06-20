fbb9e094681721a8368ad194b57342ddb4e337eb
martinosso <103454347+martinosso@users.noreply.github.com>
2022-06-20 17:03:56 +0200

    Update earned points on add earned points to employees migration

--- db/migrate/20220609183321_add_earned_points_to_employees.rb
+++ db/migrate/20220609183321_add_earned_points_to_employees.rb
@@ -1,5 +1,19 @@
 class AddEarnedPointsToEmployees < ActiveRecord::Migration[6.1]
-  def change
+  def up
     add_column :employees, :earned_points, :integer, null: false, default: 0
+    set_earned_points
+  end
+  
+  def down
+    remove_column :employees, :earned_points
+  end
+
+  private
+
+  def set_earned_points
+    Employee.includes(:received_kudos).all.each do |employee|
+      received_kudos = employee.received_kudos.size
+      employee.update_attribute(:earned_points, received_kudos * 100) if received_kudos > 0
+    end
   end
 end
