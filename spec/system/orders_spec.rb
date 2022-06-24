2162fac15d708cec4a2a586e4b037208f306cc28
martinosso <103454347+martinosso@users.noreply.github.com>
2022-06-24 02:30:36 +0200

    Create orders tests

---
+++ spec/system/orders_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'orders test process' do
+  let(:employee) { create(:employee, earned_points: 200) }
+  let!(:reward) { create(:reward) }
+
+  before do
+    sign_in employee
+    visit rewards_path
+  end
+
+  it 'creates order' do
+    click_link 'Buy now'
+    expect(page).to have_content("Earned points: #{(100 * (employee.earned_points - reward.price)).to_i}")
+  end
+end
